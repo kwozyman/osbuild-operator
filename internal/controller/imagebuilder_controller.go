d738d59235723901deeeede2e74deb6fb93ca295
Costin Gamenț <costin.gament@gmail.com>
2023-07-13 13:28:18 +0200

    remove logger example

--- internal/controller/imagebuilder_controller.go
+++ internal/controller/imagebuilder_controller.go
@@ -47,8 +47,6 @@ type ImageBuilderReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
 func (r *ImageBuilderReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
 	logger := log.FromContext(ctx)
 	logger.Info("hello operator")
 
